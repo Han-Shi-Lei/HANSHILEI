702c3c39e27249bfbfe44bd0d85f3f8cd30bf426
Han-Shi-Lei <1245160877@qq.com>
2018-11-05 21:09:44 +0800

    提交代码

---
+++ text_11_2/text_11_2/test.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS    1
+#include <stdlib.h>
+#include <stdio.h>
+//int check_sys()
+//{
+//	int i = 1;
+//	return (*(char*)&i);
+//}
+	unsigned char i = 0;
+
+int main()
+{
+	/*int a = 20;
+	int b = -10;*/
+	/*int ret = check_sys();
+	if (1 == ret)
+	{
+		printf("Ð¡¶Ë\n");
+	}
+	if (0 == ret)
+	{
+		printf("´ó¶Ë\n");
+	}*/
+
+	/*char a = -128;*/
+	/*signed char b = -1;
+	unsigned char c = -1;*/
+	/*printf("a = %u\n",a);*/
+   /* int ;*/
+	/*unsigned int i = 10;*/
+	/*printf("%d\n",i+j);*/
+	/*for(i=9; i >= 0;i--)
+	{
+		printf("%u\n",i);
+	}*/
+
+	/*char a[1000];
+	int i;
+	for(i=0; i<1000; i++)
+	{
+		a[i] = -1-i;
+	}
+	printf("%d",strlen(a));*/
+	/*short num = 32767;
+	short int a = num+1;
+    printf("%d",a);*/
+
+	for(i=0; i <= 255;i++)
+	{
+		printf("hello world\n");
+	}
+	
+	system ("pause");
+	return 0;
+}
